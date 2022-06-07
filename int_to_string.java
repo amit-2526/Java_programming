66d71062b326a86a9a0db7fa68920c256947d3a9
Amit Kumar Yadav <96667638+amit-2526@users.noreply.github.com>
2022-06-07 18:10:12 +0530

    Create int_to_string.java

---
+++ int_to_string.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class int_to_string {
+
+
+
+    // Integer to String
+  //by the valurOf function
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        if(n>-100 && n<100){
+            String s = String.valueOf(n);
+            System.out.println("Good job");
+        
+           /*  {
+            
+                System.out.println("WRONG");
+            } */
+
+        }
+        }
+    
+}
